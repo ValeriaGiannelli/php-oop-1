f8e6702e432788a2496a44294c6d39345a2b9619
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-03 14:17:21 +0200

    feat: creat class Movie and add variables and the function construct

--- Model/Movie.php
+++ Model/Movie.php
@@ -2,5 +2,18 @@
 
 // qui inserisco la classe Movie con le proprietà - metodi - costruttore
 class Movie {
-    
+    public $title;
+    public $year;
+    public $duration;
+    public $genre;
+
+    // questa potrebbe essere una composizione
+    public $cast;
+
+    // costruttore
+    public function __construct($_title, $_year, $_duration){
+        $this -> title = $title;
+        $this -> year = $year;
+        $this -> duration = $duration;
+    }
 }
