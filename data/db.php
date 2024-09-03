d6eeac2c29e2984e62dba6544adbd194e92cf2d6
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-03 15:30:08 +0200

    feat(bonus): add the new class to the db of the movies

--- data/db.php
+++ data/db.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../Model/Cast.php';
 // qui creao eventuale database per inserire le istanze della classe Movie
 
 $movies = [
-    new Movie('Una pallottola spuntata', '1988', 86, ['comico', 'poliziesco']),
-    new Movie('White chicks', '2004', 109, ['comico']),
-    new Movie('Scary Movie 3', '2003', 84, ['comico', 'commedia']),
+    new Movie('Una pallottola spuntata', '1988', 86, ['comico', 'poliziesco'], new Cast('Leslie', 'Nielsen'), new Cast('Priscilla', 'Presley')),
+    new Movie('White chicks', '2004', 109, ['comico'], new Cast('Marlon', 'Wayans'), new Cast('Shawn', 'Wayans')),
+    new Movie('Scary Movie 3', '2003', 84, ['comico', 'commedia'], new Cast('Anna', 'Faris'), new Cast('Charlie', 'Sheen')),
 ];
